4462cc34ab9fdbcda371b7b726b0671523dd3e4e
Anuj Shelke <anujshelke18@gmail.com>
2026-07-23 22:33:02 +0530

    Code for modulus operator operations

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello World")
+
+a = int(input("Enter number 1 : "))
+b = int(input("Enter number 2 : "))
+c = (a%b)
+c = print("The remainder of the number when divided by any number would ",c)
