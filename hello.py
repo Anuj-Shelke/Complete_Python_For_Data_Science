5492dfee9def9ac41916f99d66bfcc3d2375870f
Anuj Shelke <anujshelke18@gmail.com>
2026-07-23 22:37:34 +0530

    Python program to print avg of two numbers

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-
+#Python program to print avg of two numbers 
 a = int(input("Enter number 1 : "))
 b = int(input("Enter number 2 : "))
-c = (a>b)
-c = print("Code to check if A is greater than B  ",c)
+c = (a+b)
+avg = c/2
+c = print("The avg of the two numbers enered is ",avg)
