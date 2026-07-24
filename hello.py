6a544efba12b19be135c60a69794529738d56f68
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 11:51:57 +0530

    #Program to check if student is pass or fail ;

--- hello.py
+++ hello.py
@@ -1,15 +1,10 @@
-#Program to find Greatest of four numbers Entered by the user ;
-a = int(input('Enter number : ' ))
-b = int(input('Enter number 2 : '))
-c = int(input('Enter number 3 : '))
-d = int(input("Enter number 4 : "))
-if(a>b and a>c and a>d):
-    print("A is greatest among the four")
-elif(b>c and b>d):
-    print("B is greatest among the four ")
-elif(c>d):
-    print("C is greatest among the four")
+#Program to check if student is pass or fail ;
+maths = int(input("Enter marks of Maths"))
+chem = int(input("Enter marks of Chem"))
+phy = int(input("Enter marks of Physics "))
+
+if(chem >= 33 and phy >= 33 and maths >= 33):
+    if((maths+chem+phy/3)>=40):
+        print("You are pass ")
 else:
-    print("D is greatest")
-
-
+    print("Sorry try again next time")
