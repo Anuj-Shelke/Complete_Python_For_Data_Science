5e9fa323a580f0db3b97bfa60f88b15c17d4b0ba
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 22:36:48 +0530

    #Program to find greatest of three numbers using function  ;

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
-#Program for understanding Functions in python ;
-def greeting(name):
-    print("Hello ",name)
+#Program to find greatest of three numbers using function  ;
+def greatest_of_three_numbers(a,b,c):
+    if(a>b and a >c):
+        print("A is the greatest number ")
+    elif(b>c):
+        print("B is the greatest number")
+    else:
+        print("C is the greatest number ")
 
-greeting("Anuj")
+a = int(input("Enter number 1"))
+b = int(input("Enter number 2"))
+c = int(input("Enter number 3"))
+greatest_of_three_numbers(a,b,c)
+
+
+
 
 
     
