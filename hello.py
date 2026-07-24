1fbcddc0494a264c752842b3fb4fa83e582c45fa
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 11:44:31 +0530

    Program to find Greatest of four numbers Entered by the user

--- hello.py
+++ hello.py
@@ -1,14 +1,15 @@
-#Conditional Statement ;
-age = int(input("Enter Your Age"))
-if(age>18):
-    print("Approved You are above the age of 18")
-elif(age ==0):
-    print("You have entered zero")
-elif(age<0):
-    print("this is a invalid age")
-else : 
-    print("Declined You are not above the age of 18")
-
-print("End of the Program")
+#Program to find Greatest of four numbers Entered by the user ;
+a = int(input('Enter number : ' ))
+b = int(input('Enter number 2 : '))
+c = int(input('Enter number 3 : '))
+d = int(input("Enter number 4 : "))
+if(a>b and a>c and a>d):
+    print("A is greatest among the four")
+elif(b>c and b>d):
+    print("B is greatest among the four ")
+elif(c>d):
+    print("C is greatest among the four")
+else:
+    print("D is greatest")
 
 
