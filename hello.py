00b5b1725253f0f5d60a2973df764d24d0cc339a
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 11:32:08 +0530

    Conditional Statement Using Elif

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@
 age = int(input("Enter Your Age"))
 if(age>18):
     print("Approved You are above the age of 18")
+elif(age ==0):
+    print("You have entered zero")
+elif(age<0):
+    print("this is a invalid age")
 else : 
     print("Declined You are not above the age of 18")
 
