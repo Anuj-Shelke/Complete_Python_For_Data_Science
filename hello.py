1b4e4ef5870a8c9f1c9714349ffdf679485d6aff
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 12:15:34 +0530

    #Program to give grade based on marks  ;

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
-#Program to check Spam or not spam ;
-text = input("Enter your message")
-
-if(text == "Make a lot of Money" or text ==  "buy now" or text ==  "click this"):
-        print("Spam ")
-else:
-    print("Not Spam")
+#Program to give grade based on marks  ;
+marks = int(input("Enter Your marks to know Grade"))
+if(marks >= 90 and marks<=100):
+    print("Grade A")
+elif(marks > 80 and marks<90):
+    print("Grade B")
+elif(marks >= 70 and marks<=80):
+    print("Grade C")
+elif(marks >= 60 and marks<70):
+    print("Grade D")
+elif(marks >= 50 and marks<=60):
+    print("Grade E")
+elif(marks >= 40  and marks<=50):
+    print("Grade F")
+else :
+    print("You are Fail")
