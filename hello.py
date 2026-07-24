666356d3eb4c68a270fb62beebe96dcfb101e5f2
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 11:58:09 +0530

    #Program to check Spam or not spam ;

--- hello.py
+++ hello.py
@@ -1,10 +1,7 @@
-#Program to check if student is pass or fail ;
-maths = int(input("Enter marks of Maths"))
-chem = int(input("Enter marks of Chem"))
-phy = int(input("Enter marks of Physics "))
+#Program to check Spam or not spam ;
+text = input("Enter your message")
 
-if(chem >= 33 and phy >= 33 and maths >= 33):
-    if((maths+chem+phy/3)>=40):
-        print("You are pass ")
+if(text == "Make a lot of Money" or text ==  "buy now" or text ==  "click this"):
+        print("Spam ")
 else:
-    print("Sorry try again next time")
+    print("Not Spam")
