e9697748fa097d5b71af5136f0f68345654f51e1
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 22:42:28 +0530

    #Program to print hello without auto nextline function in python ;

--- hello.py
+++ hello.py
@@ -1,16 +1,6 @@
-#Program to find greatest of three numbers using function  ;
-def greatest_of_three_numbers(a,b,c):
-    if(a>b and a >c):
-        print("A is the greatest number ")
-    elif(b>c):
-        print("B is the greatest number")
-    else:
-        print("C is the greatest number ")
-
-a = int(input("Enter number 1"))
-b = int(input("Enter number 2"))
-c = int(input("Enter number 3"))
-greatest_of_three_numbers(a,b,c)
+#Program to print hello without auto nextline function in python ;
+print("HEllo ",end="" )
+print("World")
 
 
 
