2bf68b6563329d9a2b6a1cf2d7e6859903c7aea4
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 22:52:55 +0530

    #Program to print sum of first n natural numbers Using recursive function  ;

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
-#Program to print hello without auto nextline function in python ;
-print("HEllo ",end="" )
-print("World")
-
+#Program to print sum of first n natural numbers Using recursive function  ;
+def sum(n):
+   if(n==1):
+      return 1
+   return sum(n-1)+n
+n = int(input("Enter your number"))
+print("sum of first n natural numbers is ",sum(n))
+      
 
 
 
