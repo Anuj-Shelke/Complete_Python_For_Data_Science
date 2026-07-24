91368b344b3dda9e1bdff7e572a2c69d3f6081c7
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 11:28:28 +0530

    Conditional Statements Firt Code

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
-#Write a program to give Different inputs to same key and see what happens
-d ={}
-name = input("Enter Your name")
-lang = input("Enter Your Language")
-d.update({name:lang})
-name = input("Enter Your name")
-lang = input("Enter Your Language")
-d.update({name:lang})
-print(d)
+#Conditional Statement ;
+age = int(input("Enter Your Age"))
+if(age>18):
+    print("Approved You are above the age of 18")
+else : 
+    print("Declined You are not above the age of 18")
+
+print("End of the Program")
 
 
