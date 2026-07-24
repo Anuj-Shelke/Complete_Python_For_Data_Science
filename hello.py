db8bebd01af0ee84da1d921bc812c8164ac42a97
Anuj Shelke <anujshelke18@gmail.com>
2026-07-24 12:24:55 +0530

    #Program to Check if the given name is present in List   ;

--- hello.py
+++ hello.py
@@ -1,16 +1,8 @@
-#Program to give grade based on marks  ;
-marks = int(input("Enter Your marks to know Grade"))
-if(marks >= 90 and marks<=100):
-    print("Grade A")
-elif(marks > 80 and marks<90):
-    print("Grade B")
-elif(marks >= 70 and marks<=80):
-    print("Grade C")
-elif(marks >= 60 and marks<70):
-    print("Grade D")
-elif(marks >= 50 and marks<=60):
-    print("Grade E")
-elif(marks >= 40  and marks<=50):
-    print("Grade F")
-else :
-    print("You are Fail")
+#Program to Check if the given name is present in List   ;
+li = ["Anuj","Amit","Rahul","Yash"]
+name = input("Enter the name you want to search for ")
+if(name in li):
+    print("Present in list")
+else:
+    print("not present in list")
+    
