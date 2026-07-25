3d1623396a7acec365b8cee553897ab696ac03f7
Anuj Shelke <anujshelke18@gmail.com>
2026-07-25 10:23:10 +0530

    File I/O in Python

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
-#Program to print sum of first n natural numbers Using recursive function  ;
-def sum(n):
-   if(n==1):
-      return 1
-   return sum(n-1)+n
-n = int(input("Enter your number"))
-print("sum of first n natural numbers is ",sum(n))
+#File I/O in Python 
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
       
 
 
