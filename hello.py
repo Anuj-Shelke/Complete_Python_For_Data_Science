6c93dd7dbc1cf2747522b77f21e7381d621bcb45
Anuj Shelke <anujshelke18@gmail.com>
2026-07-25 11:45:32 +0530

    #Write a program to print multiplication table from 2 to 20 and store it in different files place this foler in files

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
-#Write a program to read from file poem.txt and find if twinkle word is present in the file 
+#Write a program to print multiplication table from 2 to 20 and store it in different files place this foler in files 
 
-with open("poem.txt") as f:
-    content = f.read()
-    if("Twinkle" in content):
-        print(content)
-    else:
-        print("The file does not contain the word sorry")
+def table_writer(n):
+    table =""
+    for i in range(1,11):
+        table +=(f"{i}X{n} = {i*n}\n")
+
+    with open(f"table/table_{n}", "w") as f:
+        f.write(table)
+
+
+
+for i in range(1,21):
+    table_writer(i)
+
+
 
 
 
