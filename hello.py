a618a874327e0f02f5f3d1b0d6560550d500658f
Anuj Shelke <anujshelke18@gmail.com>
2026-09-02 19:08:18 +0530

    Starting with LinkCode Technology Full Stack Python

--- hello.py
+++ hello.py
@@ -1,22 +1,5 @@
-#OOPs in Python 
-class programmer:
-    def __init__(self,n):
-        self.n = n 
+print("Hello world ")
 
-    def square(self):
-        print("The square of the number is :",self.n*self.n)
-    def cube(self):
-        print("The cube of the number is :",self.n*self.n*self.n)
-    def square_root(self):
-        print("The square root of num is :",self.n**0.5)
-    
-    
-print("Enter number to get all the parameters of :")
-n= int(input("Enter the number "))
-num = programmer(n)
-num.square()
-num.cube()
-num.square_root()
 
 
 
